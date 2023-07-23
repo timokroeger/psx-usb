039cd02007e619bbfac61be1ad5ac8293f627073
Timo Kröger <timokroeger93@gmail.com>
2023-07-23 20:28:46 +0200

    Restructure code for clarity

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use core::pin::pin;
 use defmt::*;
 use embassy_futures::join::join;
 use embassy_rp::gpio::{Level, Output, Pull};
-use embassy_rp::pio::{self, Direction, Pio, ShiftConfig, ShiftDirection};
+use embassy_rp::pio::{self, Direction, IrqFlags, Pio, ShiftConfig, ShiftDirection, StateMachine};
 use embassy_rp::relocate::RelocatedProgram;
 use embassy_rp::{bind_interrupts, peripherals, usb, Peripheral};
 use embassy_time::{with_timeout, Duration, Ticker};
@@ -28,16 +28,6 @@ bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => usb::InterruptHandler<peripherals::USB>;
     PIO0_IRQ_0 => pio::InterruptHandler<peripherals::PIO0>;
 });
-
-// #[derive(Debug, Clone, Copy)]
-// #[repr(packed)]
-// struct ControllerData {
-//     buttons: u16,
-//     rx: u8,
-//     ry: u8,
-//     x: u8,
-//     y: u8,
-// }
 
 fn build_hid_controller<'d, D: Driver<'d>>(
     builder: &mut embassy_usb::Builder<'d, D>,
@@ -101,12 +91,116 @@ async fn run_controller(mut c: Controller<'_>, data: &ControllerData) -> ! {
     }
 }
 
+async fn poll_psx(
+    sm0: &mut StateMachine<'_, peripherals::PIO0, 0>,
+    irq_flags: &IrqFlags<'_, peripherals::PIO0>,
+    rx_dma: &mut peripherals::DMA_CH0,
+    tx_dma: &mut peripherals::DMA_CH1,
+) -> Option<[u8; 32]> {
+    sm0.clear_fifos();
+    irq_flags.set(0); // ACK flag
+    sm0.set_enable(true);
+
+    let (rx, tx) = sm0.rx_tx();
+
+    let command = [
+        0x01_u8, // controller (not memory card)
+        0x42,    // poll
+        0x01,    // multitap
+        0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 1
+        0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 2
+        0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 3
+        0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 4
+    ];
+    let tx_fut = tx.dma_push(tx_dma.into_ref(), &command);
+
+    let mut response = [0x0_u8; 35];
+    let rx_fut = rx.dma_pull(rx_dma.into_ref(), &mut response);
+
+    let _ = with_timeout(Duration::from_micros(1800), join(tx_fut, rx_fut)).await;
+    //debug!("{=[u8]:X}", response);
+
+    sm0.set_enable(false);
+
+    if response[1] == 0x80 && response[2] == 0x5A {
+        Some(unwrap!(response[3..32].try_into()))
+    } else {
+        None
+    }
+}
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let p = embassy_rp::init(Default::default());
+    let mut p = embassy_rp::init(Default::default());
+
+    // Configure PIO state machine for the custom PSX SPI protocol
+    let Pio {
+        mut common,
+        irq_flags,
+        mut sm0,
+        mut sm1,
+        ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    // Setup up the pins for PIO.
+    let sck_clk = common.make_pio_pin(p.PIN_2);
+    let txd_mosi = common.make_pio_pin(p.PIN_3);
+    let mut rxd_miso = common.make_pio_pin(p.PIN_4);
+    rxd_miso.set_pull(Pull::Up);
+    let mut dsr_ack = common.make_pio_pin(p.PIN_6);
+    dsr_ack.set_pull(Pull::Up);
+
+    // SM0 acts as a master SPI peripheral which waits for IRQ0 to be set before
+    // clocking out the next byte.
+    let psx_spi = pio_proc::pio_file!("src/psx.pio", select_program("spi"));
+    let psx_spi = RelocatedProgram::new(&psx_spi.program);
+    let psx_spi_len = psx_spi.code().count();
+    let mut config = pio::Config::default();
+    config.use_program(&common.load_program(&psx_spi), &[&sck_clk]);
+    config.set_in_pins(&[&rxd_miso]);
+    config.set_out_pins(&[&txd_mosi]);
+    // 1MHz PIO clock gives SPI frequency of 250kHz
+    config.clock_divider = (U56F8!(125_000_000) / U56F8!(1_000_000)).to_fixed();
+    // LSB first
+    config.shift_in = ShiftConfig {
+        threshold: 32,
+        direction: ShiftDirection::Right,
+        auto_fill: true,
+    };
+    config.shift_out = ShiftConfig {
+        threshold: 8,
+        direction: ShiftDirection::Right,
+        auto_fill: false,
+    };
+    sm0.set_config(&config);
+    sm0.set_pin_dirs(Direction::Out, &[&sck_clk, &txd_mosi]);
+    // The pio_file() proc macro fails to compile the instruction
+    // `mov x, !null` to set the register X to 0xFFFFFFFF.
+    // Execute to compiled version of this instruction as a workaround.
+    unsafe { sm0.exec_instr(0xa02b) };
+
+    // SM1 continuously monitors the ACK line and sets IRQ1 if there is an acknowledge.
+    let psx_ack = pio_proc::pio_file!("src/psx.pio", select_program("ack"));
+    let psx_ack = RelocatedProgram::new_with_origin(&psx_ack.program, psx_spi_len as u8);
+    let mut config = pio::Config::default();
+    config.use_program(&common.load_program(&psx_ack), &[]);
+    config.set_in_pins(&[&dsr_ack]);
+    sm1.set_config(&config);
+    sm1.set_enable(true);
+
+    // Use a GPIO as output to control the chip select line.
+    let mut dtr_cs = Output::new(p.PIN_5, Level::High);
+
+    // Configure the USB stack
     let driver = usb::Driver::new(p.USB, Irqs);
 
-    // Create embassy-usb Config
+    // The state must be defined before the builder for correct drop order.
+    // Otherwise the compiler fails to see that the state lives long enough.
+    let mut s0 = hid::State::new();
+    let mut s1 = hid::State::new();
+    let mut s2 = hid::State::new();
+    let mut s3 = hid::State::new();
+
     let mut config = embassy_usb::Config::new(0x0000, 0x0000);
     config.device_release = 0x0000;
     config.manufacturer = Some("Timo Kröger");
@@ -114,17 +208,10 @@ fn main() -> ! {
     config.max_power = 500;
     config.max_packet_size_0 = 64;
 
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
     let mut device_descriptor = [0; 20];
     let mut config_descriptor = [0; 112];
     let mut bos_descriptor = [0; 12];
     let mut control_buf = [0; 64];
-
-    let mut s0 = hid::State::new();
-    let mut s1 = hid::State::new();
-    let mut s2 = hid::State::new();
-    let mut s3 = hid::State::new();
 
     let mut builder = embassy_usb::Builder::new(
         driver,
@@ -134,7 +221,6 @@ fn main() -> ! {
         &mut bos_descriptor,
         &mut control_buf,
     );
-
     let c0 = build_hid_controller(&mut builder, &mut s0);
     let c1 = build_hid_controller(&mut builder, &mut s1);
     let c2 = build_hid_controller(&mut builder, &mut s2);
@@ -142,114 +228,37 @@ fn main() -> ! {
 
     let mut usb = builder.build();
 
-    let data = &[CONTROLLER_DATA_INIT; 4];
-
     // USB
     let usb = pin!(async { usb.run().await });
 
+    // Controller data shawed between tasks
+    let controller_data = &[CONTROLLER_DATA_INIT; 4];
+
     // Controllers
-    let c0 = pin!(async move { run_controller(c0, &data[0]).await });
-    let c1 = pin!(async move { run_controller(c1, &data[1]).await });
-    let c2 = pin!(async move { run_controller(c2, &data[2]).await });
-    let c3 = pin!(async move { run_controller(c3, &data[3]).await });
+    let c0 = pin!(async move { run_controller(c0, &controller_data[0]).await });
+    let c1 = pin!(async move { run_controller(c1, &controller_data[1]).await });
+    let c2 = pin!(async move { run_controller(c2, &controller_data[2]).await });
+    let c3 = pin!(async move { run_controller(c3, &controller_data[3]).await });
 
     // PSX protocol: SPI with LSB first and special ack signal
     let psx_spi = pin!(async move {
-        let Pio {
-            mut common,
-            irq_flags,
-            mut sm0,
-            mut sm1,
-            ..
-        } = Pio::new(p.PIO0, Irqs);
-
-        let sck_clk = common.make_pio_pin(p.PIN_2);
-        let txd_mosi = common.make_pio_pin(p.PIN_3);
-        let mut rxd_miso = common.make_pio_pin(p.PIN_4);
-        rxd_miso.set_pull(Pull::Up);
-        let mut dtr_cs = Output::new(p.PIN_5, Level::High);
-        let mut dsr_ack = common.make_pio_pin(p.PIN_6);
-        dsr_ack.set_pull(Pull::Up);
-
-        let psx_spi = pio_proc::pio_file!("src/psx.pio", select_program("spi"));
-        let psx_spi = RelocatedProgram::new(&psx_spi.program);
-        let psx_spi_len = psx_spi.code().count();
-        let mut config = pio::Config::default();
-        config.use_program(&common.load_program(&psx_spi), &[&sck_clk]);
-        config.set_in_pins(&[&rxd_miso]);
-        config.set_out_pins(&[&txd_mosi]);
-        // 1MHz PIO clock gives SPI frequency of 250kHz
-        config.clock_divider = (U56F8!(125_000_000) / U56F8!(1_000_000)).to_fixed();
-        // LSB first
-        config.shift_in = ShiftConfig {
-            threshold: 32,
-            direction: ShiftDirection::Right,
-            auto_fill: true,
-        };
-        config.shift_out = ShiftConfig {
-            threshold: 8,
-            direction: ShiftDirection::Right,
-            auto_fill: false,
-        };
-        sm0.set_config(&config);
-        sm0.set_pin_dirs(Direction::Out, &[&sck_clk, &txd_mosi]);
-        // Set register X to 0xFFFFFFFF with `mov x, !null`
-        unsafe { sm0.exec_instr(0xa02b) };
-
-        let psx_ack = pio_proc::pio_file!("src/psx.pio", select_program("ack"));
-        let psx_ack = RelocatedProgram::new_with_origin(&psx_ack.program, psx_spi_len as u8);
-        let mut config = pio::Config::default();
-        config.use_program(&common.load_program(&psx_ack), &[]);
-        config.set_in_pins(&[&dsr_ack]);
-        sm1.set_config(&config);
-        sm1.set_enable(true);
-
         let mut led = Output::new(p.PIN_25, Level::Low);
-
-        let mut rx_dma = p.DMA_CH0.into_ref();
-        let mut tx_dma = p.DMA_CH1.into_ref();
 
         let mut ticker = Ticker::every(Duration::from_millis(2));
         loop {
             ticker.next().await;
-
             dtr_cs.set_low();
+            let poll_data = poll_psx(&mut sm0, &irq_flags, &mut p.DMA_CH0, &mut p.DMA_CH1).await;
+            dtr_cs.set_high();
 
-            sm0.clear_fifos();
-            irq_flags.set(0); // ACK flag
-            sm0.set_enable(true);
-
-            let (rx, tx) = sm0.rx_tx();
-
-            let command = [
-                0x01_u8, // controller (not memory card)
-                0x42,    // poll
-                0x01,    // multitap
-                0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 1
-                0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 2
-                0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 3
-                0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 4
-            ];
-            let tx_fut = tx.dma_push(tx_dma.reborrow(), &command);
-
-            let mut response = [0x0_u8; 35];
-            let rx_fut = rx.dma_pull(rx_dma.reborrow(), &mut response);
-
-            let _ = with_timeout(Duration::from_micros(1800), join(tx_fut, rx_fut)).await;
-            //debug!("{=[u8]:X}", response);
-
-            if response[1] == 0x80 && response[2] == 0x5A {
-                let response = &response[3..];
-                data[0].set(response[2..8].try_into().unwrap());
-                data[1].set(response[10..16].try_into().unwrap());
-                data[2].set(response[18..24].try_into().unwrap());
-                data[3].set(response[26..32].try_into().unwrap());
+            if let Some(poll_data) = poll_data {
+                controller_data[0].set(poll_data[2..8].try_into().unwrap());
+                controller_data[1].set(poll_data[10..16].try_into().unwrap());
+                controller_data[2].set(poll_data[18..24].try_into().unwrap());
+                controller_data[3].set(poll_data[26..32].try_into().unwrap());
 
                 led.toggle();
             }
-
-            dtr_cs.set_high();
-            sm0.set_enable(false);
         }
     });
 
